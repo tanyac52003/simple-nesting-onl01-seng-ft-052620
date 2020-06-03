4482c934c4901cf639e23791cec4fa462c6c2a97
Tanya Hodges <tanyac52003@gmail.com>
2020-06-03 02:37:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,8 @@ def adding_matz
         }
      }
 
-    programmer_hash << [:yukihiro_matsumoto], 
-      :known_for => "Ruby",
+    programmer_hash << [:yukihiro_matsumoto] 
+      :known_for => "Ruby"
       :languages => ["LISP", "C"]
     
 end
