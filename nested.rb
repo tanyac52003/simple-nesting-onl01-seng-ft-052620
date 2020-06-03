91c607f17d3b51231f95676a1e055f0adbe1e514
Tanya Hodges <tanyac52003@gmail.com>
2020-06-03 03:20:41 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,11 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
+  
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].join()  
 end
 
 def adding_matz
@@ -80,7 +84,11 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+    programmer_hash
 end
 
 def changing_alan
@@ -99,11 +107,16 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
+  end 
+    
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
      
-end
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -121,5 +134,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages][1] = ["C", "Assembly"] 
+programmer_hash
 end
