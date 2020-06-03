6f8043b70734278c9301d04e75861d6d459d098d
Tanya Hodges <tanyac52003@gmail.com>
2020-06-03 03:11:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,12 +109,13 @@ def changing_alan
      
      programmer_hash[:alan_kay][:known_for] = "GUI"
      programmer_hash
+  end 
     
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
      
-end
+
 
 def adding_to_dennis
 	programmer_hash = 
