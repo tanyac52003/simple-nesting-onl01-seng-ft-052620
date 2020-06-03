c027687e29a9f46b98f2eaa1ddc712c7d70c0872
Tanya Hodges <tanyac52003@gmail.com>
2020-06-03 02:54:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,9 +106,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
+    
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
